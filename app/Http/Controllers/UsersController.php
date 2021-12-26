9bde091ea7016f58b3ebca89dc98b9f1af4ac9d0
mrcr_geek <mrcrsogand@gmail.com>
2021-12-26 17:46:55 +0330

    Complete Register Method For User

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-    public function register()
+    public function register(Request $request)
     {
+        $request->validate([
+            'name' => 'required|alpha_num|unique:App\Models\User',
+            'lastname' => 'required',
+            'phoneNumber' => 'required|numeric|unique:App\Models\User',
+            'password' => 'required'
+        ]);
 
+        $register_data = [
+            'name' => $request->input('name'),
+            'lastname' => $request->input('lastname'),
+            'phoneNumber' => $request->input('phoneNumber'),
+            'password' => Hash::make($request->input('password'))
+        ];
+
+        if(!\Check_PhoneNum_ir($register_data['phoneNumber']))
+        {
+            return response()->json([
+                'message' => 'Phone number should not be more or less than 11 char & should be IR'
+            ], 422);
+        }
+
+        User::create($register_data);
+
+        return response()->json([
+           'message' => 'register was successful'
+        ], 200);
     }
 }
