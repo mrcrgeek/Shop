0d1c2edb0ec4376321a5db1687c0278d0eeb08c0
mrcr_geek <mrcrsogand@gmail.com>
2021-12-26 14:47:15 +0330

    Create users table

--- database/migrations/2021_12_26_110301_create_users_table.php
+++ database/migrations/2021_12_26_110301_create_users_table.php
@@ -15,11 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 200);
+            $table->string('lastname', 200);
+            $table->string('phoneNumber');
+            $table->string('password', 400);
             $table->timestamps();
         });
     }
