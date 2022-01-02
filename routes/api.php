d6fcebcc8c20274c46806b2fca459c023b2dff43
mrcr_geek <mrcrsogand@gmail.com>
2022-01-02 18:04:05 +0330

    Sorting Routes & Change Some of Them

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,25 @@ use App\Http\Controllers\AdminsController;
 */
 
 Route::prefix('/user')->group(function () {
-    Route::post('/register', [UsersController::class, 'register'])->name('signup_route');
-    Route::post('/login', [UsersController::class, 'login'])->name('login_route');
-    Route::post('/edit_profile', [UsersController::class, 'edit_profile'])->middleware('auth:users')->name('edit_route');
-    Route::get('/info', [UsersController::class, 'show'])->middleware('auth:users')->name('test_middleware');
+    Route::post('/register', [UsersController::class, 'register'])
+        ->name('signup_route');
+    Route::post('/login', [UsersController::class, 'login'])
+        ->name('login_route');
+    Route::post('/edit_profile', [UsersController::class, 'edit_profile'])->middleware('auth:users')
+        ->name('edit_route');
+    Route::get('/info', [UsersController::class, 'show'])->middleware('auth:users')
+        ->name('test_middleware');
 });
 
 Route::prefix('/admin')->group(function () {
-    Route::post('/login', [AdminsController::class, 'login'])->name('admin_login');
-    Route::get('/all_users', [AdminsController::class, 'all_users'])->middleware('auth:admins')->name('all_users');
-    Route::get('/users', [AdminsController::class, 'User_Paginate'])->middleware('auth:admins')->name('user_pagination');
-    Route::post('/edit_User/{id}', [AdminsController::class, 'edit_user'])->middleware('auth:admins')->name('edit_user');
-    Route::get('/info', [AdminsController::class, 'show'])->middleware('auth:admins')->name('test_middleware');
+    Route::post('/login', [AdminsController::class, 'login'])
+        ->name('admin_login');
+    Route::get('/all_users', [AdminsController::class, 'all_users'])->middleware('auth:admins')
+        ->name('all_users');
+    Route::get('/users', [AdminsController::class, 'User_Paginate'])->middleware('auth:admins')
+        ->name('user_pagination');
+    Route::post('/edit_user/{id}', [AdminsController::class, 'edit_user'])->middleware('auth:admins')
+        ->name('edit_user');
+    Route::get('/info', [AdminsController::class, 'show'])->middleware('auth:admins')
+        ->name('test_middleware');
 });
