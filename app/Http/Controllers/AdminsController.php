c0b337fbb09522f3f5d59632a0e3831ac014d6bf
mrcr_geek <mrcrsogand@gmail.com>
2022-01-02 18:02:39 +0330

    Complete Edit User (by admin) Method

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -86,4 +86,42 @@ class AdminsController extends Controller
             'message' => 'No Users found'
         ], 404);
     }
+
+    public function edit_user(Request $request,$id)
+    {
+        $request->validate([
+            'name' => 'max:55|unique:App\Models\User',
+            'lastname' => 'max:100',
+            'phoneNumber' => 'numeric|unique:App\Models\User',
+            'password' => 'max:400'
+        ]);
+
+        $User_object = User::where('id', $id);
+
+        if($User_object->exists()) {
+            $New_Data = [];
+
+            (!empty($request->input('name')) && ($New_Data['name'] = $request->input('name')));
+            (!empty($request->input('lastname')) && ($New_Data['lastname'] = $request->input('lastname')));
+            (!empty($request->input('phoneNumber')) && ($New_Data['phoneNumber'] = $request->input('phoneNumber')));
+            (!empty($request->input('password')) && ($New_Data['password'] = Hash::make($request->input('password'))));
+
+            if (isset($New_Data['phoneNumber']) && (!\Check_PhoneNum_ir($request->input('phoneNumber'))))
+            {
+                return response()->json([
+                    'message' => 'Phone number should not be more or less than 11 char & should be IR'
+                ], 422);
+            }
+
+            $User_object->update($New_Data);
+
+            return response()->json([
+                'message' => 'Update Was Successful'
+            ],200);
+        }
+
+        return response()->json([
+            'message' => 'User Not Found'
+        ], 404);
+    }
 }
