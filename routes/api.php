f70f021fd555eb49dd96313d641585a1fe64712e
mrcr_geek <mrcrsogand@gmail.com>
2022-01-05 23:45:00 +0330

    Add Update Product Route

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::prefix('/user')->group(function () {
         ->name('signup_route');
     Route::post('/login', [UsersController::class, 'login'])
         ->name('login_route');
-    Route::post('/edit_profile', [UsersController::class, 'edit_profile'])->middleware('auth:users')
+    Route::patch('/edit_profile', [UsersController::class, 'edit_profile'])->middleware('auth:users')
         ->name('edit_route');
     Route::get('/info', [UsersController::class, 'show'])->middleware('auth:users')
         ->name('test_middleware');
@@ -34,12 +34,14 @@ Route::prefix('/admin')->group(function () {
         ->name('all_users');
     Route::get('/users', [AdminsController::class, 'User_Paginate'])->middleware('auth:admins')
         ->name('user_pagination');
-    Route::post('/edit_user/{id}', [AdminsController::class, 'edit_user'])->middleware('auth:admins')
+    Route::patch('/edit_user/{id}', [AdminsController::class, 'edit_user'])->middleware('auth:admins')
         ->name('edit_user');
     Route::post('/product/add_category', [AdminsController::class, 'add_category'])->middleware('auth:admins')
         ->name('add_category');
-    Route::post('product/add', [AdminsController::class, 'add_product'])->middleware('auth:admins')
+    Route::post('/product/add', [AdminsController::class, 'add_product'])->middleware('auth:admins')
         ->name('add_product');
+    Route::post('/product/update/{id}', [AdminsController::class, 'update_product'])->middleware('auth:admins')
+        ->name('update_product');
     Route::get('/info', [AdminsController::class, 'show'])->middleware('auth:admins')
         ->name('test_middleware');
 });
