cab52324da91bf32fb155d7e84d1a14e78210ed8
mrcr_geek <mrcrsogand@gmail.com>
2022-01-05 19:46:35 +0330

    Add Products Migration

---
+++ database/migrations/2022_01_05_155823_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->string('description', 150);
+            $table->bigInteger('price');
+            $table->string('img');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
